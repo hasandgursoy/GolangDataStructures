3b60ed5cb70baea491ae49c7138d2f3962a56a2d
hasandgursoy <hasan.d.gursoy@gmail.com>
2023-03-15 09:07:43 +0300

    Added : Pointer

--- pointers.go
+++ pointers.go
@@ -4,21 +4,39 @@ import (
 	"fmt"
 )
 
-func pointers() {
-	a, b := 56, 97
+type Circle struct {
+	Radius float64
+}
 
-	fmt.Println(b, a)
+func (c Circle) Area() float64 {
+	c.Radius = 10 // struct örneği üzerinde kalıcı değişiklik yapmaz
+	return 3.14 * c.Radius * c.Radius
+}
 
-	i := 0
-	for i < 20 {
-		i++
-	}
+func (c *Circle) AreaPointer() float64 {
+	c.Radius = 10 // struct örneği üzerinde kalıcı değişiklik yapar
+	return 3.14 * c.Radius * c.Radius
+}
 
-	var mehmetali float64 = 1
+func main() {
+	c1 := Circle{Radius: 5}
+	c2 := Circle{Radius: 5}
 
-	fmt.Println(mehmetali)
+	fmt.Println("Before calling any method")
+	fmt.Println("c1:", c1.Radius) // c1: 5
+	fmt.Println("c2:", c2.Radius) // c2: 5
 
-	// 0x14000014070 = 56
-	// 0x14000014070 = 97
+	fmt.Println("After calling Area method")
+	c1.Area()                     // struct kopyası üzerinde işlem yapar, değişiklikleri kaydetmez
+	fmt.Println("c1:", c1.Radius) // c1: 5
 
+	fmt.Println("After calling AreaPointer method")
+	c2.AreaPointer()              // struct üzerinde doğrudan işlem yapar, değişiklikleri kaydeder
+	fmt.Println("c2:", c2.Radius) // c2: 10
+
+	/*
+			main() fonksiyonunda, önce c1 ve c2 isimli iki adet Circle struct'ı örneği oluşturulur. Daha sonra, her iki struct örneği üzerinde de önce Area() metodu çağrılır. Area() metodunun çağrıldığı c1 struct'ının örneği üzerinde bir değişiklik yapılmadığı görülür. Ancak, AreaPointer() metodunun çağrıldığı c2 struct'ının örneği üzerinde Radius özelliği 10'a ayarlanır ve bu değişiklik kalıcı olur.
+
+		Sonuç olarak, fonksiyon alıcısında pointer kullanmak, struct örneği üzerinde kalıcı değişiklikler yapmamızı sağlar.
+	*/
 }
